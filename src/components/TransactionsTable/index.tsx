cdb6b93c91236b14424e3a23586757aadafea63c
Guilherme <gui_pmendes@hotmail.com>
2021-04-19 00:56:05 -0300

    forms

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,39 +1,52 @@
-import { useEffect } from "react";
+import { transitions } from "polished";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
-export function TransactionsTable(){
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
 
-    useEffect(()=> {
-        api.get('transactions')
-        .then(response=>console.log(response.data))
-    },[])
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título</th>
-                        <th>Preço</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de sistemas</td>
-                        <td className='deposit'>R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>ALuguel</td>
-                        <td className='withdraw'>- R$1.100</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>{new Intl.DateTimeFormat('pt-br').format(new Date(transaction.createdAt))}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,52 @@
-import { createServer, Model } from 'miragejs';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './App';
+import { createServer, Model } from "miragejs";
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
 
 createServer({
   models: {
-    transaction: Model
+    transaction: Model,
   },
 
-  routes(){
-    this.namespace= 'api';
-    this.get('transactions', () => {
-      return this.schema.all('transaction')
-    })
-      this.post('/transaction', (schema, resquest)=>{
-        const data = JSON.parse(resquest.requestBody)
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "frelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-12-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-12-14 19:00:00"),
+        },
+      ],
+    });
+  },
 
-        return(schema.create('trasaction', data))
-      } )
-  }
-})
+  routes() {
+    this.namespace = "api";
+    this.get("transactions", () => {
+      return this.schema.all("transaction");
+    });
+    this.post("/transaction", (schema, resquest) => {
+      const data = JSON.parse(resquest.requestBody);
+
+      return schema.create("trasaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
